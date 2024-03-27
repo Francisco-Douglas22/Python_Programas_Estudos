887c5138ea3b4c03fa30d400f4e0119ac223f707
Francisco Douglas <franciscodouglas77@outlook.com>
2024-03-27 13:44:54 -0300

    -----

--- Teste.py
+++ Teste.py
@@ -1,1 +1,18 @@
-print('Seu nome e')
+#Strings em Python
+
+curso = 'Python Developer'
+
+# Maiusculo, Minuscula, Titulo
+print("Maiusculo: ", curso.upper())
+print('Minusculo: ', curso.lower())
+print('Primeira Maiuscula: ', curso.title())
+
+#Eliminando Espaços em Branco
+curso = " Python Developer "
+print('Remove todos os espacos: ', curso.strip())
+print('Remove espacos da Esquerda: ', curso.lstrip())
+print('Remove espaco do Lado Direito: ', curso.rstrip())
+
+#Junçao e Centralizacao
+print("Centraliza: ", curso.center(10, '#'))
+print('Juncao: ', ".".join(curso))
